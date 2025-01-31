55c04176394d6eddc5350c812a42f109c96a07f8
lauraverchere <163655337+lauraverchere@users.noreply.github.com>
2025-01-31 21:20:57 +0100

    Update producer-projet.py

--- producer-projet.py
+++ producer-projet.py
@@ -6,10 +6,13 @@ import time
 # ---------------------------------------------------------------------------
 # Étape 1 : Configuration
 # ---------------------------------------------------------------------------
+# TODO : Remplacez par votre clé personnelle d'API OpenWeather
 API_KEY = '60c351069138b06a8e6cc9b06d8c4752'
 
+# TODO : Ajouter deux autres villes de votre choix pour atteindre 5 villes
 CITIES = ['Paris', 'London', 'Tokyo', 'Kuala Lumpur', 'Annecy']  # Exemple : Ajoutez ici vos villes
 
+# TODO : Remplacez par le nom du topic Kafka utilisé dans votre projet
 KAFKA_TOPIC = 'topic-weather'
 
 # Adresse du serveur Kafka
@@ -19,8 +22,8 @@ KAFKA_SERVER = 'localhost:9092'
 # Étape 2 : Initialisation du producteur Kafka
 # ---------------------------------------------------------------------------
 producer = KafkaProducer(
-    bootstrap_servers=[KAFKA_SERVER],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Sérialisation en JSON
+    bootstrap_servers=[KAFKA_SERVER], # Adresse à laquelle le producer doit se connecter
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Sérialisation/Conversion en JSON puis encodage en binaire
 )
 
 # ---------------------------------------------------------------------------
@@ -33,8 +36,8 @@ def get_weather_data(city):
     :return: Données JSON de la météo
     """
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
-    return response.json() if response.status_code == 200 else None
+    response = requests.get(url) #Récupération des données depuis l'API
+    return response.json() if response.status_code == 200 else None # Si requête API réussi (code 200), on récupère les données (et on les converti en un format lisible par python) sinon on retourne rien
 
 # ---------------------------------------------------------------------------
 # Étape 4 : Envoi des données en continu vers Kafka
@@ -42,7 +45,7 @@ def get_weather_data(city):
 while True:
     for city in CITIES:
         data = get_weather_data(city)  # Récupération des données météo
-        if data:
+        if data: # si des données ont été récupérées (!= None) alors données envoyées au topic Kafka)
             producer.send(KAFKA_TOPIC, key=city.encode('utf-8'), value=data)  # Envoi au topic Kafka
             print(f"Données envoyées pour {city}: {data}")
     time.sleep(60)  # Pause d'une minute entre chaque envoi
