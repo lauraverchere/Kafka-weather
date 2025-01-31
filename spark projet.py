7fd7c0e14e1ae346a6a642337058035b6210ba96
lauraverchere <163655337+lauraverchere@users.noreply.github.com>
2025-01-31 19:37:42 +0100

    Update spark projet.py

--- spark projet.py
+++ spark projet.py
@@ -2,14 +2,13 @@ import os
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType, ArrayType
 from pyspark.sql.functions import (
-    col, from_json, from_unixtime, to_timestamp, expr, to_json, struct
+    col, from_json, from_unixtime, to_timestamp, expr, to_json, struct, when, hour
 )
 
 # ---------------------------------------------------------------------------
 # Étape 1 : Configuration de Spark
 # ---------------------------------------------------------------------------
-# TODO : Modifier "tp-esme" par le nom de votre répertoire GitHub ou chemin local
-os.environ["SPARK_HOME"] = "/workspaces/tp-esme/spark-3.2.3-bin-hadoop2.7"
+os.environ["SPARK_HOME"] = "/workspaces/projet-esme/spark-3.2.3-bin-hadoop2.7"
 
 spark = SparkSession.builder \
     .appName("KafkaWeatherConsumer") \
@@ -46,13 +45,13 @@ weather_schema = StructType([
 # ---------------------------------------------------------------------------
 # Étape 3 : Lecture des données en streaming depuis Kafka
 # ---------------------------------------------------------------------------
-# TODO : Remplacer "tp-meteo" par le nom du topic que vous souhaitez utiliser
 df = (
     spark.readStream
     .format("kafka")
     .option("kafka.bootstrap.servers", "localhost:9092")
-    .option("subscribe", "tp-meteo")
+    .option("subscribe", "topic-weather")
     .option("startingOffsets", "earliest")
+    .option("failOnDataLoss", "false")
     .load()
 )
 
@@ -81,19 +80,41 @@ processed_df = parsed_df.select(
 # Étape 5 : Création de nouvelles variables
 # ---------------------------------------------------------------------------
 
-# TODO : Ajouter une colonne "heat_index" avec la formule :
+# Ajout d'une colonne "heat_index" avec la formule :
+
 # température + (0.5555 * ((6.11 * (10 ^ ((7.5 * température) / (237.7 + température))) * (humidité / 100)) - 10))
+processed_df = processed_df.withColumn(
+    "heat_index",
+    processed_df.temperature +
+        (0.5555 * ((6.11 * ((10** ((7.5 * processed_df.temperature) / (237.7 + processed_df.temperature)))) * (processed_df.humidity / 100)) - 10))
+)
 
-# TODO : Ajouter une colonne "severity_index" avec la formule :
+
+#  Ajout d'une colonne "severity_index" avec la formule :
 # (vitesse du vent * 0.5) + ((1015 - pression) * 0.3) + (humidité * 0.2)
+processed_df = processed_df.withColumn(
+    "severity_index",
+    (processed_df.speed * 0.5) + ((1015 - processed_df.pressure) * 0.3) + (processed_df.humidity * 0.2)
+)
+ 
+ 
+#  Ajout d'une colonne "time_of_day" pour catégoriser la période de la journée :
 
-# TODO : Ajouter une colonne "time_of_day" pour catégoriser la période de la journée :
 # Matin (6h-12h), Après-midi (12h-18h), Soirée (18h-24h), Nuit (0h-6h)
+processed_df = processed_df.withColumn(
+    "time_of_day",
+    when(hour(processed_df['date']) >= 6, 'Matin')
+    .when(hour(processed_df['date']) >= 12, 'Après-midi')
+    .when(hour(processed_df['date']) >= 18, 'Soirée')
+    .otherwise('Nuit') 
+)
+
+
+
 
 # ---------------------------------------------------------------------------
 # Étape 6 : Transformation des données en JSON pour Kafka
 # ---------------------------------------------------------------------------
-# Todo: ajouter toutes les variables calculer
 kafka_output_df = processed_df.select(
     to_json(struct(
         col("date"),
@@ -112,13 +133,12 @@ kafka_output_df = processed_df.select(
 # ---------------------------------------------------------------------------
 # Étape 7 : Écriture des résultats dans un topic Kafka
 # ---------------------------------------------------------------------------
-# TODO : Remplacer "tp-meteo-final" par le nom du topic Kafka où envoyer les données
 query = (kafka_output_df
     .writeStream
     .format("kafka")
     .outputMode("append")
     .option("kafka.bootstrap.servers", "localhost:9092")  # Adresse du broker Kafka
-    .option("topic", "tp-meteo-final")                   # Nom du topic Kafka de destination
+    .option("topic", "topic-weather-final")                   # Nom du topic Kafka de destination
     .option("checkpointLocation", "/tmp/checkpoints")    # Emplacement des checkpoints
     .start())
 
